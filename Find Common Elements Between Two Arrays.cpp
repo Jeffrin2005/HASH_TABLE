4fc3d0efa2349d29f162ea2612db220bb57b1620
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-08-28 12:12:34 +0530

    Update Find Common Elements Between Two Arrays.cpp

--- Find Common Elements Between Two Arrays.cpp
+++ Find Common Elements Between Two Arrays.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// MAP SOLUTION 
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
         map<int, int> count1;
         map<int, int> count2;
@@ -33,3 +34,29 @@ public:
         return {answer1, answer2};
     }
 };
+// SET SOLUTION
+
+
+class Solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        set<int> set1(nums1.begin(), nums1.end());
+        set<int> set2(nums2.begin(), nums2.end());
+        int answer1 = 0;
+        for (auto&num : nums1){
+            if (set2.count(num)){
+                answer1++;
+            }
+        }
+        int answer2 = 0;
+        for (auto&num : nums2){
+            if (set1.count(num)){
+                answer2++;
+            }
+        }
+        
+        return {answer1, answer2};
+    }
+};
+
+
